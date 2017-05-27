7615bffc6c2e6c22def42c82ac63b110f7c9efb3
bwilliams <benj@minwilliams.net>
2017-05-27 16:52:32 +1000

    update movement tests properly test full array being returned

--- src/components/Robot/movement.test.js
+++ src/components/Robot/movement.test.js
@@ -6,7 +6,7 @@ describe('movement', ()=>{
     expect(movement.moveIsValid(0,0)).toEqual(true);
     expect(movement.moveIsValid(1,1)).toEqual(true);
     expect(movement.moveIsValid(2,2)).toEqual(true);
-    expect(movement.moveIsValid(3,3)).toEqual(true);
+    expect(movement.moveIsValid(3,2)).toEqual(true);
     expect(movement.moveIsValid(4,4)).toEqual(true);
   });
   it('should NOT allow invalid moves', ()=>{
@@ -16,13 +16,31 @@ describe('movement', ()=>{
     expect(movement.moveIsValid(100,100)).toEqual(false);
   });
   it('should move north correctly', ()=>{
-    expect(movement.moveRobot(0,0,'N')).toEqual(0,1,'N');
-    expect(movement.moveRobot(1,1,'N')).toEqual(1,2,'N');
-    expect(movement.moveRobot(3,3,'N')).toEqual(3,4,'N');
+    expect(movement.moveRobot([0,0,'N'])).toEqual([0,1,'N']);
+    expect(movement.moveRobot([1,1,'N'])).toEqual([1,2,'N']);
+    expect(movement.moveRobot([3,3,'N'])).toEqual([3,4,'N']);
+  });
+  it('should move north correctly', ()=>{
+    expect(movement.moveRobot([2,2,'N'])).toEqual([2,3,'N']);
+  });
+  it('should move east correctly', ()=>{
+    expect(movement.moveRobot([2,2,'E'])).toEqual([3,2,'E']);
+  });
+  it('should move south correctly', ()=>{
+    expect(movement.moveRobot([2,2,'S'])).toEqual([2,1,'S']);
+  });
+
+  it('should move west correctly', ()=>{
+    expect(movement.moveRobot([2,2,"W"])).toEqual([1,2,'W']);
   });
   it('should return the same position if not a valid move', ()=>{
-    expect(movement.moveRobot(5,5,'N')).toEqual(5,5,'N');
-    expect(movement.moveRobot(1,5,'N')).toEqual(1,5,'N');
+    expect(movement.moveRobot([5,5,'N'])).toEqual([5,5,'N']);
+    expect(movement.moveRobot([1,5,'N'])).toEqual([1,5,'N']);
+    expect(movement.moveRobot([0,0,'S'])).toEqual([0,0,'S']);
+    expect(movement.moveRobot([0,0,'W'])).toEqual([0,0,'W']);
+  });
+  it('should return input if invalid', ()=>{
+    expect(movement.moveRobot('test')).toEqual('test');
   });
 
 });
